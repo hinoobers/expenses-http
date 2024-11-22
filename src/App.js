2fa6def548530453ab55c69819e83e4cb6c737a8
Reio <reio.poldmaa@voco.ee>
2024-11-22 09:14:55 +0200

    Structure the ExpensiveItem

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 
 const App = () => {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 1),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2025, 1, 10),
+      title: 'Old book',
+      price: 12.99
+    }
+  ]
+
   return (
     <div className='App'>
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
     </div>
   );
 }
