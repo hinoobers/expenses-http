9cdfea5c3bce478045ce887fa03549d78c12125a
Reio <reio.poldmaa@voco.ee>
2024-12-02 10:27:46 +0200

    Ylesanne 13

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import './App.css'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense';
 import ExpensesFilter from './components/Expenses/ExpensesFilter';
+import { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    date: new Date(2024, 10, 1),
+    title: 'New book',
+    price: 30.99,
+    id: 'id1'
+  },
+  {
+    date: new Date(2025, 1, 10),
+    title: 'Old book',
+    price: 12.99,
+    id: 'id2'
+  },
+  {
+    date: new Date(2024, 0, 25),
+    title: 'New bag',
+    amount: 139.99,
+    id: 'id3'
+  }
+]
 
 const App = () => {
-  const DUMMY_EXPENSES = [
-    {
-      date: new Date(2024, 10, 1),
-      title: 'New book',
-      price: 30.99,
-      id: 'id1'
-    },
-    {
-      date: new Date(2025, 1, 10),
-      title: 'Old book',
-      price: 12.99,
-      id: 'id2'
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In app.js')
-    console.log(expense)
+    setExpenses((previous) => {
+      return [expense, ...previous];
+    })
   }
 
   function filterExpense(target) {
@@ -31,7 +41,7 @@ const App = () => {
   return (
     <div className='App'>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={DUMMY_EXPENSES}/>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
